e0dc09608f185e2e1be59d67f9ff4595671804fa
Kenan Amundsen Elkoca <kenan@tcmanagement.no>
2022-06-13 16:15:37 +0200

    Added button click to get products

--- inventory-forms/Form1.Designer.cs
+++ inventory-forms/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ListProducts = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListProducts
+            // 
+            this.ListProducts.Location = new System.Drawing.Point(288, 77);
+            this.ListProducts.Name = "ListProducts";
+            this.ListProducts.Size = new System.Drawing.Size(133, 23);
+            this.ListProducts.TabIndex = 0;
+            this.ListProducts.Text = "List Products";
+            this.ListProducts.UseVisualStyleBackColor = true;
+            this.ListProducts.Click += new System.EventHandler(this.ListProducts_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ListProducts);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button ListProducts;
     }
 }
--- inventory-forms/Form1.cs
+++ inventory-forms/Form1.cs
@@ -1,10 +1,20 @@
-namespace inventory_forms
+using inventory_api_wrapper;
+using inventory_api_wrapper.Models;
+
+namespace inventory_forms;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    InventoryApiWrapper _client;
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        _client = new InventoryApiWrapper();
+        InitializeComponent();
+    }
+
+    private async void ListProducts_Click(object sender, EventArgs e)
+    {
+        List<Products?>? a = await _client.ListProduct();
+        //dataGridView1.DataSource = a;
     }
 }
