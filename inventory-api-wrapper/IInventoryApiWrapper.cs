7ed3b83902f0c972399049f60d6893c7816aabd3
Kenan Amundsen Elkoca <kenan@elkoca.com>
2022-06-19 22:41:51 +0200

    Updated Inventory Api Wrapper

--- inventory-api-wrapper/IInventoryApiWrapper.cs
+++ inventory-api-wrapper/IInventoryApiWrapper.cs
@@ -5,9 +5,29 @@ namespace inventory_api_wrapper;
 
 public interface IInventoryApiWrapper
 {
+    #region Products
     public Task<GetProductListResponse?> ListProduct();
     public Task<GetProductResponse?> GetProduct(Guid productId);
     public Task<GetProductResponse?> CreateProduct(PostProductBody newProduct);
     public Task UpdateProduct(PutProductBody replaceProduct);
     public Task DeleteProduct(Guid productId);
+    #endregion
+
+    #region Vendors
+    public Task<GetVendorListResponse?> ListVendor();
+    public Task<GetVendorResponse?> GetVendor(Guid vendorId);
+    public Task<GetVendorResponse?> CreateVendor(PostVendorBody newVendor);
+    public Task UpdateVendor(PutVendorBody replaceVendor);
+    public Task DeleteVendor(Guid vendorId);
+    #endregion
+
+    #region ProductTypes
+    public Task<GetProductTypeListResponse?> ListProductType();
+    public Task<GetProductTypeResponse?> GetProductType(Guid productTypeId);
+    public Task<GetProductTypeResponse?> CreateProductType(PostProductTypeBody newProductType);
+    public Task UpdateProductType(PutProductTypeBody replaceProductType);
+    public Task DeleteProductType(Guid productTypeId);
+
+    #endregion
+
 }
